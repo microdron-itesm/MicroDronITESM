26c14c429b7253cf2bc1e55fc24436501f53573e
Alberto Jahuey <albertojahueymoncada@gmail.com>
2019-03-01 16:55:36 -0600

    Updated app.c

--- MicroDron/firmware/src/app.c
+++ MicroDron/firmware/src/app.c
@@ -1,177 +1,233 @@
-/*******************************************************************************
-  MPLAB Harmony Application Source File
-  
-  Company:
-    Microchip Technology Inc.
-  
-  File Name:
-    app.c
-
-  Summary:
-    This file contains the source code for the MPLAB Harmony application.
-
-  Description:
-    This file contains the source code for the MPLAB Harmony application.  It 
-    implements the logic of the application's state machine and it may call 
-    API routines of other MPLAB Harmony modules in the system, such as drivers,
-    system services, and middleware.  However, it does not call any of the
-    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
-    the modules in the system or make any assumptions about when those functions
-    are called.  That is the responsibility of the configuration-specific system
-    files.
- *******************************************************************************/
-
-// DOM-IGNORE-BEGIN
-/*******************************************************************************
-Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
-
-Microchip licenses to you the right to use, modify, copy and distribute
-Software only when embedded on a Microchip microcontroller or digital signal
-controller that is integrated into your product or third party product
-(pursuant to the sublicense terms in the accompanying license agreement).
-
-You should refer to the license agreement accompanying this Software for
-additional information regarding your rights and obligations.
-
-SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
-EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
-MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
-IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
-CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
-OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
-INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
-CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
-SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
-(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
- *******************************************************************************/
-// DOM-IGNORE-END
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Included Files 
-// *****************************************************************************
-// *****************************************************************************
-
 #include "app.h"
+#include <stdint.h> 
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include <stdio.h>
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Global Data Definitions
-// *****************************************************************************
-// *****************************************************************************
-
-// *****************************************************************************
-/* Application Data
-
-  Summary:
-    Holds application data
-
-  Description:
-    This structure holds the application's data.
-
-  Remarks:
-    This structure should be initialized by the APP_Initialize function.
-    
-    Application strings and buffers are be defined outside this structure.
-*/
+unsigned char readcode;
+unsigned char readyIMU = '\0';
+unsigned char arreglo[20] = {'\0'};
+unsigned char buffer[10] = {'\0'};
+int dutycycle0 = 2000;
+int dutycycle1 = 2000;
+int dutycycle2 = 2000;
+int dutycycle3 = 2000;
+float yaw, roll, pitch; //en verdad es float
+int result = 0;
+int x = 0;
+int i;
 
 APP_DATA appData;
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Callback Functions
-// *****************************************************************************
-// *****************************************************************************
-
-/* TODO:  Add any necessary callback functions.
-*/
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Local Functions
-// *****************************************************************************
-// *****************************************************************************
-
-
-/* TODO:  Add any necessary local functions.
-*/
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Initialization and State Machine Functions
-// *****************************************************************************
-// *****************************************************************************
-
-/*******************************************************************************
-  Function:
-    void APP_Initialize ( void )
-
-  Remarks:
-    See prototype in app.h.
- */
-
-void APP_Initialize ( void )
-{
-    /* Place the App state machine in its initial state. */
+void APP_Initialize ( void ){
+    DRV_TMR0_Start();
+    DRV_TMR1_Start();
+    DRV_USART0_Initialize();
+    DRV_USART1_Initialize();
+    DRV_OC0_Start ();
+    DRV_OC1_Start ();
+    DRV_OC2_Start ();
+    DRV_OC3_Start ();
+    //DRV_OC0_PulseWidthSet (1250);
+    //DRV_OC1_PulseWidthSet (2500);
+    //DRV_OC2_PulseWidthSet (3750);
+    //DRV_OC3_PulseWidthSet (5000);
     appData.state = APP_STATE_INIT;
-
-    
-    /* TODO: Initialize your application's state machine and other
-     * parameters.
-     */
+    LATAbits.LATA2 = 0b0;
+    TRISAbits.TRISA2 = 0b0;
 }
 
-
-/******************************************************************************
-  Function:
-    void APP_Tasks ( void )
-
-  Remarks:
-    See prototype in app.h.
- */
-
-void APP_Tasks ( void )
-{
-
+void APP_Tasks ( void ){
     /* Check the application's current state. */
-    switch ( appData.state )
-    {
+    switch ( appData.state ){
         /* Application's initial state. */
-        case APP_STATE_INIT:
-        {
+        case APP_STATE_INIT:{
             bool appInitialized = true;
-       
-        
-            if (appInitialized)
-            {
-            
+            if (appInitialized){
                 appData.state = APP_STATE_SERVICE_TASKS;
             }
-            break;
         }
+        break;
+        case APP_STATE_SERVICE_TASKS:{
+            DRV_TMR1_CounterClear ();
+            while(1){
+                if(!DRV_USART0_ReceiverBufferIsEmpty()){
+                    readcode=DRV_USART0_ReadByte();
+                }
+                switch (readcode){
+                    case 'U' :{
+                        dutycycle0 = 5000;
+                        DRV_OC0_PulseWidthSet(dutycycle0);
+                        DRV_OC1_PulseWidthSet (dutycycle0);
+                        DRV_OC2_PulseWidthSet (dutycycle0);
+                        DRV_OC3_PulseWidthSet (dutycycle0);
+                        readcode='\0';
+                    }
+                    break;
+                    case 'D' :{
+                        dutycycle0 = 0;
+                        DRV_OC0_PulseWidthSet (dutycycle0);
+                        DRV_OC1_PulseWidthSet (dutycycle0);
+                        DRV_OC2_PulseWidthSet (dutycycle0);
+                        DRV_OC3_PulseWidthSet (dutycycle0);
+                        readcode='\0';
+                    }
+                    break;
+                    case 'L' :{
+                        dutycycle0 = 5000;
+                        dutycycle1 = 0;
+                        DRV_OC0_PulseWidthSet (dutycycle0);
+                        DRV_OC1_PulseWidthSet (dutycycle0);
+                        DRV_OC2_PulseWidthSet (dutycycle1);
+                        DRV_OC3_PulseWidthSet (dutycycle1);
+                        readcode='\0';
+                    }
+                    break;
+                    case 'R' :{
+                        dutycycle0 = 5000;
+                        dutycycle1 = 0;
+                        DRV_OC0_PulseWidthSet (dutycycle1);
+                        DRV_OC1_PulseWidthSet (dutycycle1);
+                        DRV_OC2_PulseWidthSet (dutycycle0);
+                        DRV_OC3_PulseWidthSet (dutycycle0);
+                        readcode='\0';
+                    }
+                    break;
+                    case 'A' :{
+                        dutycycle0 -= 500;
+                        if(dutycycle0 < 0)
+                        {
+                            dutycycle0 = 0;
+                        }
+                        dutycycle1 = 0;
+                        DRV_OC0_PulseWidthSet (dutycycle1);
+                        DRV_OC1_PulseWidthSet (dutycycle1);
+                        DRV_OC2_PulseWidthSet (dutycycle0);
+                        DRV_OC3_PulseWidthSet (dutycycle0);
+                        readcode='\0';
+                    }
+                    break;
+                        case 'S' :{
+                        dutycycle0 -= 500;
+                        if(dutycycle0 < 0)
+                        {
+                            dutycycle0 = 0;
+                        }
+                        dutycycle1 = 0;
+                        DRV_OC0_PulseWidthSet (dutycycle0);
+                        DRV_OC1_PulseWidthSet (dutycycle0);
+                        DRV_OC2_PulseWidthSet (dutycycle1);
+                        DRV_OC3_PulseWidthSet (dutycycle1);
+                        readcode='\0';
+                    }
+                    break;
+                        case 'B' :{
+                        dutycycle0 += 500;
+                        if(dutycycle0 > 5000)
+                        {
+                            dutycycle0 = 5000;
+                        }
+                        dutycycle1 = 0;
+                        DRV_OC0_PulseWidthSet (dutycycle1);
+                        DRV_OC1_PulseWidthSet (dutycycle1);
+                        DRV_OC2_PulseWidthSet (dutycycle0);
+                        DRV_OC3_PulseWidthSet (dutycycle0);
+                        readcode='\0';
+                    }
+                    break;
+                    case 'C' :{
+                        dutycycle0 += 500;
+                        if(dutycycle0 > 5000)
+                        {
+                            dutycycle0 = 5000;
+                        }
+                        dutycycle1 = 0;
+                        DRV_OC0_PulseWidthSet (dutycycle0);
+                        DRV_OC1_PulseWidthSet (dutycycle0);
+                        DRV_OC2_PulseWidthSet (dutycycle1);
+                        DRV_OC3_PulseWidthSet (dutycycle1);
+                        readcode='\0';
+                    }
+                    break;
+                    
+                    default:{
+                    }
+                    break;
+                }
+                
+                if(!DRV_USART1_ReceiverBufferIsEmpty())
+                {
+                    readyIMU=DRV_USART1_ReadByte();
+                   arreglo[x]=readyIMU;
+                   x++;
 
-        case APP_STATE_SERVICE_TASKS:
-        {
-        
-            break;
+                   if(readyIMU == '\n')
+                   {
+                   result = sscanf(arreglo + 1, "%f %f %f", &yaw, &pitch, &roll);
+            
+                   
+                   x = 0;
+                   unsigned char buffer1[10] = {'\0'};
+                   unsigned char buffer2[10] = {'\0'};
+                   unsigned char buffer3[10] = {'\0'};
+                   int ret = snprintf(buffer1, sizeof buffer1, "%f", yaw);
+                       ret = snprintf(buffer2, sizeof buffer2, "%f", pitch);
+                       ret = snprintf(buffer3, sizeof buffer3, "%f", roll);
+                   
+                       for(i = 0; i<10 ; i++)
+                       {
+                            DRV_USART0_WriteByte(buffer1[i]);
+                       }
+                       DRV_USART0_WriteByte(' '); 
+                               
+                       for(i = 0; i<10 ; i++)
+                       {
+                            DRV_USART0_WriteByte(buffer2[i]);
+                       }
+                       
+                       DRV_USART0_WriteByte(' ');
+                       
+                       for(i = 0; i<10 ; i++)
+                       {
+                            DRV_USART0_WriteByte(buffer3[i]);
+                       }                       
+                       
+                       DRV_USART0_WriteByte('\n');
+                       memset(arreglo,0,sizeof arreglo);
+                       memset(buffer,0,sizeof buffer);
+                   }
+                   
+                   //DRV_USART0_WriteByte(yaw);
+                 
+                }
+                switch (readyIMU){
+                    case 'k' :{
+                        
+                    }
+                    break;
+                    default:{
+                        
+                    }
+                    break;
+                }
+                /*while (DRV_TMR0_CounterValueGet() < 10000){
+                }
+                DRV_TMR1_CounterClear ();
+                PORTAbits.RA2 = 0b1;
+                while (DRV_TMR0_CounterValueGet() < 10000){
+                }
+                DRV_TMR1_CounterClear ();
+                PORTAbits.RA2 = 0b0;*/
+            }
         }
+        break;
 
         /* TODO: implement your application state machine.*/
         
 
         /* The default state should never be executed. */
-        default:
-        {
+        default:{
             /* TODO: Handle error in application's state machine. */
-            break;
         }
+        break;
     }
 }
-
- 
-
-/*******************************************************************************
- End of File
- */
